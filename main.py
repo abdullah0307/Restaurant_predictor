8d444b1f27e80bad73a0e8d2ed60ab7fb3f9d4e9
Hamza's_Workstation <abdullahjavaid0307@gmail.com>
2021-01-25 23:35:43 +0500

    3rd commit 25/1/2020

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import csv
+import re
+
 from PyQt5 import QtWidgets
 
 from GUI import Ui_MainWindow
@@ -28,17 +31,77 @@ class Main:
         # Buttons connection
         self.main_obj.pushButton_2.clicked.connect(lambda: self.register_window.show())
         self.main_obj.pushButton.clicked.connect(self.login)
+        self.register_obj.pushButton.clicked.connect(self.register)
+
+    def encode(self, password):
+        encode = ""
+        for i in password:
+            encode = encode + chr(ord(i) + 18)
+        return encode
+
+    def decode(self, password):
+        decode = ""
+        for i in password:
+            decode = decode + chr(ord(i) - 18)
+        return decode
+
+    def user_exists(self, email):
+        users = pd.read_csv("data/users.csv")
+        for i in users['email']:
+            if i == email:
+                return True
+        return False
 
     def check_user(self, email, password):
         users = pd.read_csv("data/users.csv")
         for i in users['email']:
             if i == email:
-                if users[users['email'] == i]['password'] == password:
+                password = self.encode(password)
+                if users[users['email'] == i]['password'].values[0] == password:
                     return True
         return False
 
+    def check_valid_email(self, email):
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        # pass the regular expression
+        # and the string in search() method
+        if re.search(regex, email):
+            return True
+        else:
+            return False
+
     def register(self):
-        pass
+        email = self.register_obj.lineEdit.text()
+        password = self.register_obj.lineEdit_2.text()
+        confirm_password = self.register_obj.lineEdit_3.text()
+
+        if email is "" or password is "" or confirm_password is "":
+            self.warning_obj.label_2.setText("Please fill all the field")
+            self.warning_window.show()
+            return
+        elif not self.check_valid_email(email):
+            self.warning_obj.label_2.setText("Email is not valid")
+            self.warning_window.show()
+            return
+        elif password != confirm_password:
+            self.warning_obj.label_2.setText("Passwords don't matched")
+            self.warning_window.show()
+            return
+
+        elif self.user_exists(email):
+            self.warning_obj.label_2.setText("User already exists")
+            self.warning_window.show()
+            return
+
+        password = self.encode(password)
+
+        # create a new user entry
+        row = [email, password]
+        with open("data/users.csv", "a") as f:
+            wo = csv.writer(f, lineterminator='\n')
+            wo.writerow(row)
+
+        self.register_window.destroy()
 
     def login(self):
         input_email = self.main_obj.lineEdit.text()
